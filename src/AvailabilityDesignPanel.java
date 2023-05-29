570e12f0377cf710f2104a4936179a55790eb88d
Thijmen <thijmtang2003@gmail.com>
2023-05-29 20:37:53 +0200

    [availability design]
    - componts overview
    - backtracking integration

--- src/AvailabilityDesignPanel.java
+++ src/AvailabilityDesignPanel.java
@@ -3,6 +3,7 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
@@ -39,45 +40,63 @@ public class AvailabilityDesignPanel extends JPanel implements ActionListener {
         laatOntwerpZien = new JButton("Laat ontwerp zien");
         laatOntwerpZien.addActionListener(this);
 
+
         subPanel.add(beschikbaarheidsPercentage);
         subPanel.add(beschikbaarheidsPercentageNummer);
         subPanel.add(laatOntwerpZien);
+
+
+        ArrayList<Component> components = ComponentRepository.findAll();
+        ComponentViewPanel componentViewPanel = new ComponentViewPanel(null, components);
+
+        add(componentViewPanel);
         add(subPanel, BorderLayout.SOUTH);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        InfrastructureDesign exampleDesign = new InfrastructureDesign(false);
 //        exampleDesign.add(new Databaseserver("DB-01"));
 //        exampleDesign.add(new Firewall("FW-01"));
 //        exampleDesign.add(new Firewall("FW-02"));
 //        exampleDesign.add(new Webserver("WS-01"));
 //        exampleDesign.add(new Webserver("WS-01"));
 //        exampleDesign.add(new Webserver("WS-02"));
+
         if (e.getSource() == laatOntwerpZien) {
             try {
                 // ophalen en een double getal van maken
                 double nummer = Double.parseDouble(beschikbaarheidsPercentageNummer.getText());
                 BigDecimal decimal = BigDecimal.valueOf(nummer);
                 int scale = decimal.scale();
+
                 if (nummer >= 0 && nummer <= 100 && scale <= 4) {
                     // maar 2 decimalen want anders rond het af
-                    String ingevoerdPercentage = String.format("%."+scale+"f", nummer);
-                    System.out.println("Ingevoerd getal: " + ingevoerdPercentage);
-//                    BacktrackAlgo algo = new BacktrackAlgo();
-//                    BacktrackAlgo.availability = nummer; //Beschikbaarheid invullen.
-//                    ArrayList<String> result = BacktrackAlgo.getServerConfiguration(); //"result" bevat nu de namen van de servers.
-//                    for (int i = 0; i < result.size(); i++) {
-//                        System.out.println(result.get(i));
-//                    }
-//                    System.out.println(algo.getBestCosts());
-                    // verder code naar volgend scherm
-                    this.mainFrame.setActiveBody(new OverviewPanel(exampleDesign,this,this.mainFrame));
+
+                    BacktrackAlgo algo = new BacktrackAlgo();
+                    BacktrackAlgo.availability = nummer / 100; //Beschikbaarheid invullen.
+                    ArrayList<String> result = BacktrackAlgo.getServerConfiguration(); //"result" bevat nu de namen van de servers.
+
+
+                    System.out.println("test");
+                    ArrayList<Component> bestConfiguration = new ArrayList<>();
+                    for(String componentName : result){
+                        // Get component using component name, disgusting
+                        Component component = new Component(componentName);
+                        bestConfiguration.add(component);
+                    }
+
+                    // Convert Components into infrastructureDesign object
+                    InfrastructureDesign infrastructureDesign = new InfrastructureDesign(false);
+                    infrastructureDesign.add(bestConfiguration);
+
+                    this.mainFrame.setActiveBody(new OverviewPanel(infrastructureDesign, this, this.mainFrame));
+
                 } else { //verkeerd getal
                     JOptionPane.showMessageDialog(this, "Voer een getal in tussen 0 en 100. \n " +
                             "Het getal mag niet meer dan vier decimalen achter de komma hebben.");
-                   beschikbaarheidsPercentageNummer.setText("");
+                    beschikbaarheidsPercentageNummer.setText("");
                 }
+
             } catch (NumberFormatException ex) { //letters ingevoerd
                 JOptionPane.showMessageDialog(this, "Ongeldige invoer. Voer een geldig getal in.");
                 beschikbaarheidsPercentageNummer.setText("");
--- src/InfrastructureDesign.java
+++ src/InfrastructureDesign.java
@@ -14,180 +14,180 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class InfrastructureDesign {
-	private ArrayList<Firewall> firewalls;
-	private ArrayList<Webserver> webservers;
-	private ArrayList<Databaseserver> databases;
-	private ArrayList<Component> components;
-	private boolean custom;
+    private ArrayList<Firewall> firewalls;
+    private ArrayList<Webserver> webservers;
+    private ArrayList<Databaseserver> databases;
+    private ArrayList<Component> components;
+    private boolean custom;
 
-	public InfrastructureDesign(boolean custom) {
-		this.custom = custom;
-		this.firewalls = new ArrayList<Firewall>();
-		this.webservers = new ArrayList<Webserver>();
-		this.databases = new ArrayList<Databaseserver>();
-	}
+    public InfrastructureDesign(boolean custom) {
+        this.custom = custom;
+        this.firewalls = new ArrayList<Firewall>();
+        this.webservers = new ArrayList<Webserver>();
+        this.databases = new ArrayList<Databaseserver>();
+    }
 
-	public InfrastructureDesign(String path) {
-		this.firewalls = new ArrayList<Firewall>();
-		this.webservers = new ArrayList<Webserver>();
-		this.databases = new ArrayList<Databaseserver>();
+    public InfrastructureDesign(String path) {
+        this.firewalls = new ArrayList<Firewall>();
+        this.webservers = new ArrayList<Webserver>();
+        this.databases = new ArrayList<Databaseserver>();
 
-		loadDesign(path);
-	}
+        loadDesign(path);
+    }
 
-	public ArrayList<Firewall> getFirewalls() {
-		return firewalls;
-	}
+    public ArrayList<Firewall> getFirewalls() {
+        return firewalls;
+    }
 
-	public ArrayList<Databaseserver> getDatabases() {
-		return databases;
-	}
+    public ArrayList<Databaseserver> getDatabases() {
+        return databases;
+    }
 
-	public ArrayList<Webserver> getWebservers() {
-		return webservers;
-	}
+    public ArrayList<Webserver> getWebservers() {
+        return webservers;
+    }
 
-	public int getTotalCost() {
-		int totalCosts = 0;
-		for (Component component : getComponents()){
-			totalCosts += component.getAnnualPriceInEuro();
-		}
+    public int getTotalCost() {
+        int totalCosts = 0;
+        for (Component component : getComponents()) {
+            totalCosts += component.getAnnualPriceInEuro();
+        }
 
-		return totalCosts;
-	}
+        return totalCosts;
+    }
 
-	public double getTotalAvailability() {
-		int totalAvailability = 0;
-		ArrayList<Component> components = getComponents();
+    public double getTotalAvailability() {
+        int totalAvailability = 0;
+        ArrayList<Component> components = getComponents();
 
-		for (Component component : components){
-			totalAvailability += component.getAvailability();
-		}
+        for (Component component : components) {
+            totalAvailability += component.getAvailability();
+        }
 
-		return totalAvailability / components.size();
-	}
-	public ArrayList<Component>  getComponents() {
-		return this.components;
-	}
+        return totalAvailability / components.size();
+    }
 
-	public void saveDesign(String path) {
-		Gson gson = new Gson();
+    public ArrayList<Component> getComponents() {
+        return this.components;
+    }
 
-		Map<String, Object> map = new HashMap<>();
+    public void saveDesign(String path) {
+        Gson gson = new Gson();
 
-		map.put("date_created", java.time.LocalDateTime.now().toString());
-		map.put("custom", this.custom);
+        Map<String, Object> map = new HashMap<>();
 
-		ArrayList<Integer> componentIds = new ArrayList<>();
-		ArrayList<Map> componentsHashmap = new ArrayList<>();
+        map.put("date_created", java.time.LocalDateTime.now().toString());
+        map.put("custom", this.custom);
 
-		// Format components into Hashmap for JSON file
-		for(int i = 0; i < components.size(); i++){
-			// Only add to known components id's if not added yet or the list is still empty
-			if(componentIds.size() > 0 || !componentIds.contains(components.get(i).getId())){
-				componentIds.add(components.get(i).getId());
-			} else{
-				continue;
-			}
+        ArrayList<Integer> componentIds = new ArrayList<>();
+        ArrayList<Map> componentsHashmap = new ArrayList<>();
 
-			int quantity = 0;
-			// Loop through the remaining components in the list and increment quantity if it is the same component
-			for(int x = i; x < components.size(); x++) {
-				Component current = components.get(i);
-				Component loop = components.get(x);
+        // Format components into Hashmap for JSON file
+        for (int i = 0; i < components.size(); i++) {
+            // Only add to known components id's if not added yet or the list is still empty
+            if (componentIds.size() > 0 || !componentIds.contains(components.get(i).getId())) {
+                componentIds.add(components.get(i).getId());
+            } else {
+                continue;
+            }
 
-				if(current.getId() == loop.getId()){
-					quantity++;
-				}
-			}
+            int quantity = 0;
+            // Loop through the remaining components in the list and increment quantity if it is the same component
+            for (int x = i; x < components.size(); x++) {
+                Component current = components.get(i);
+                Component loop = components.get(x);
 
-			Map<String, Object> componentMap = new HashMap<>();
-			componentMap.put("id", components.get(i).getId());
-			componentMap.put("quantity", quantity);
-			componentsHashmap.add(componentMap);
-		}
+                if (current.getId() == loop.getId()) {
+                    quantity++;
+                }
+            }
 
-		map.put("components", componentsHashmap);
+            Map<String, Object> componentMap = new HashMap<>();
+            componentMap.put("id", components.get(i).getId());
+            componentMap.put("quantity", quantity);
+            componentsHashmap.add(componentMap);
+        }
 
-		// Convert Hashmaps to a JSON string
-		String json = gson.toJson(map);
+        map.put("components", componentsHashmap);
 
-		try {
-			// Create a new JSON file with given Design
-			Files.write(Path.of(path + ".json"), json.getBytes());
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+        // Convert Hashmaps to a JSON string
+        String json = gson.toJson(map);
 
-
-
-	public boolean loadDesign(String path) {
-		Gson gson = new Gson();
-		ArrayList<Component> importComponents = new ArrayList<>();
-		// create a reader
-		Reader reader = null;
-		try {
-			reader = Files.newBufferedReader(Paths.get(path));
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-
-		JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
-
-			if(!jsonObject.has("custom") || !jsonObject.has("date_created") || !jsonObject.has("components") ){
-				return false;
-			}
-
-			// Copy details from json to current Object.
-			this.custom = jsonObject.get("custom").getAsBoolean();
-			JsonArray components = jsonObject.getAsJsonArray("components");
-
-			// Iterate over the nested components
-			for (JsonElement element : components) {
-				// Convert JsonElement to JsonObject to be able to access the attributes.
-				JsonObject componentJSON = element.getAsJsonObject();
-				if (componentJSON.has("id") && componentJSON.has("quantity")) {
-					int id = componentJSON.get("id").getAsInt();
-					int quantity = componentJSON.get("quantity").getAsInt();
-					Component component = new Component(id);
-
-					for (int i = 0; i < quantity; i++) {
-						importComponents.add(component);
-					}
-				}
-			}
-
-			add(importComponents);
+        try {
+            // Create a new JSON file with given Design
+            Files.write(Path.of(path + ".json"), json.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
-		return true;
-	}
+    public boolean loadDesign(String path) {
+        Gson gson = new Gson();
+        ArrayList<Component> importComponents = new ArrayList<>();
+        // create a reader
+        Reader reader = null;
+        try {
+            reader = Files.newBufferedReader(Paths.get(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-	public void add(Firewall firewall) {
-		this.firewalls.add(firewall);
-	}
+        JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
 
-	public void add(Webserver webserver) {
-		this.webservers.add(webserver);
-	}
+        if (!jsonObject.has("custom") || !jsonObject.has("date_created") || !jsonObject.has("components")) {
+            return false;
+        }
 
-	public void add(Databaseserver database) {
-		this.databases.add(database);
-	}
+        // Copy details from json to current Object.
+        this.custom = jsonObject.get("custom").getAsBoolean();
+        JsonArray components = jsonObject.getAsJsonArray("components");
 
-	public void add(ArrayList<Component> components) {
-		this.components = components;
+        // Iterate over the nested components
+        for (JsonElement element : components) {
+            // Convert JsonElement to JsonObject to be able to access the attributes.
+            JsonObject componentJSON = element.getAsJsonObject();
+            if (componentJSON.has("id") && componentJSON.has("quantity")) {
+                int id = componentJSON.get("id").getAsInt();
+                int quantity = componentJSON.get("quantity").getAsInt();
+                Component component = new Component(id);
 
-		// Convert components to component type array
-		for (Component component : components) {
-			if (component.getComponentTypesId() == Component.FIREWALL) {
-				this.add(new Firewall(component.getId()));
-			} else if (component.getComponentTypesId() == Component.DBSERVER) {
-				this.add(new Databaseserver(component.getId()));
-			} else if (component.getComponentTypesId() == Component.WEBSERVER) {
-				this.add(new Webserver(component.getId()));
-			}
-		}
-	}
+                for (int i = 0; i < quantity; i++) {
+                    importComponents.add(component);
+                }
+            }
+        }
+
+        add(importComponents);
+
+
+        return true;
+    }
+
+    public void add(Firewall firewall) {
+        this.firewalls.add(firewall);
+    }
+
+    public void add(Webserver webserver) {
+        this.webservers.add(webserver);
+    }
+
+    public void add(Databaseserver database) {
+        this.databases.add(database);
+    }
+
+    public void add(ArrayList<Component> components) {
+        this.components = components;
+
+        // Convert components to component type array
+        for (Component component : components) {
+            if (component.getComponentTypesId() == Component.FIREWALL) {
+                this.add(new Firewall(component.getId()));
+            } else if (component.getComponentTypesId() == Component.DBSERVER) {
+                this.add(new Databaseserver(component.getId()));
+            } else if (component.getComponentTypesId() == Component.WEBSERVER) {
+                this.add(new Webserver(component.getId()));
+            }
+        }
+    }
 }
